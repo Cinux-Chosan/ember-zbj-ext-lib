1b445d22731d936b25aa502e00604328b516e215
Your Name <you@example.com>
2017-05-29 23:53:59 +0800

    xx

--- addon/utils/ember.utils.js
+++ addon/utils/ember.utils.js
@@ -3,6 +3,7 @@ import { set, get, isEmber } from './__lib_utils__';
 const { defineProperty: defProp, setPrototypeOf: setProto, getPrototypeOf: getProto } = Object;
 
 let proto_array = getProto([]);
+let proto_string = getProto('');
 
 // 扩展 findBy with ==
 let symbol_findBy = Symbol('findBy with ==, not ===');
@@ -32,4 +33,37 @@ defProp(proto_array, symbol_removeBy, {
 });
 export let removeBy = symbol_removeBy;
 export let removeby = symbol_removeBy;
+
+
+// not finished
+let symbol_convertFalse = Symbol('convert false to what you want');
+defProp(proto_array, symbol_convertFalse, {
+  get() {
+    return dest => Array.from(this, el => el ? el : dest, this);
+  }
+});
+
+export let convertFalse = symbol_convertFalse;
+
+//
+let symbol_typesof = Symbol('return all types of elements');
+defProp(proto_array, symbol_typesof, {
+  get() {
+    return Array.from(this, el => typeof el);
+  }
+})
+
+export let typesOf = symbol_typesof;
+
+//
+let symbol_str_length = Symbol('return string length, Unicode friendly');
+defProp(proto_string, symbol_str_length, {
+  get() {
+    return Array.from(this).length;
+  }
+})
+
+export let str_length = symbol_str_length;
+
+
 export let setP = setProto;
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,23 +1,22 @@
 import Ember from 'ember';
 import computed from 'ember-computed-decorators';
-<<<<<<< HEAD
-import { genArray, findby } from 'ember-zbj-ext-lib';
-=======
-import { genArray, findby2, removeBy } from 'ember-zbj-ext-lib';
->>>>>>> eca350f43830771e0e93f948dce8a7555bf27ce8
+import { genArray, findby, removeBy, convertFalse, typesOf, str_length } from 'ember-zbj-ext-lib';
 
 export default Ember.Route.extend({
   number: genArray(1, 10),
   @computed('number') getN(n) {
     return n;
   },
-  arr: [{id: 1, name: 'zhang'}],
+  arr: [{id: 1, name: 'zhang'}, 0],
   model() {
     let arr = this.get('arr');
     console.log(arr[findby]('id', '1'));
     console.log(this.get('getN'));
     console.log(removeBy);
-    arr[removeBy]('id', 1);
+    // arr[removeBy]('id', 1);
+    console.log(arr[typesOf]);
+    console.log('zhang建军'[str_length]);
+    arr[convertFalse]('');
     console.log(arr);
     return null;
   }
